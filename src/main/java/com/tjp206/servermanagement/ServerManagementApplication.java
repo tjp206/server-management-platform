fcf1144ea8ceb9021046c7791bd73050ba8cf696
TJ Pataria <tjpataria@gmail.com>
2023-06-07 13:31:13 +0100

    Initial commit

---
+++ src/main/java/com/tjp206/servermanagement/ServerManagementApplication.java
@@ -0,0 +1,13 @@
+package com.tjp206.servermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tjp206/servermanagement/ServerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjp206.servermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
